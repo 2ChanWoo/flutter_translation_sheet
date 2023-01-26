3bfe9e5d275f058b0094ae7053a9fec2c9565c06
LCW <2chanwoo@groundk.com>
2023-01-26 14:17:06 +0900

    .

--- bin/main.dart
+++ bin/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter_translation_sheet/src/gsheets/gsheets.dart';
 import 'package:flutter_translation_sheet/src/runner.dart';
 import 'package:flutter_translation_sheet/src/utils/errors.dart';
 
+/// dart run [절대경로]/flutter_translation_sheet/bin/main.dart [fetch / fetchwith / getAll]
+
 /// entry point of the program.
 /// Delegates arguments to the CommandRunner.
 Future<void> main(List<String> args) async {
